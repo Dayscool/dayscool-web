730efcc5bcde9c1027418343324890adf3277aad
draco1006 <mbarragani@unal.edu.co>
2020-10-17 15:06:55 -0500

    init cursos

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },{
+  path: "/nani",
+  name: "Curso",
+    component: () =>
+      import("../views/trying.vue")
   }
 ];
 
@@ -24,3 +29,4 @@ const router = createRouter({
 });
 
 export default router;
+ 
---
+++ src/views/trying.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+    <div id="top">
+        soy top uwu
+    </div>
+    <div id=cuerpo>
+        <p>
+            Mis Cursos 
+        </p>
+        <div id="lista">
+            <Curso v-bind:cursr="{nombre: 'aa', profesor: 'sss'}" />
+        </div>
+        <div class = "boton">
+            Cursos Anteriores
+        </div>
+    </div>
+</div>
+  
+</template>
+
+<script>
+import Curso from "@/components/cours.vue";
+export default {
+    name: "cursos",
+    components: {
+        Curso
+    }
+}
+</script>
+
+<style>
+div#lista{
+    display: flex;
+    flex-direction: row ;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-content: flex-start;
+    padding: 0 15vw;
+}
+div.boton{
+    background-color: #4191E0;
+    padding: 1%;
+    width: fit-content;
+    height: fit-content;
+    justify-content: center;
+    margin: 0 auto;
+    margin-top: 4vh;
+    color: #FFFFFF;
+    border-radius: 8px;
+}
+</style>
