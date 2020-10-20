7f235af539671d61c74634f2d03b6a8122a993ea
Andres Garcia <alagarciape@unal.edu.co>
2020-10-20 10:15:50 -0500

    Added checkForm for signup and Dockerfile

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -4,25 +4,65 @@
       <div class="divcont">
         <img alt="Dayscool logo" src="../assets/logod.png" />
         <h3>Crear una cuenta de Dayscool</h3>
-        <br/>
-        <form>
-          <label >Nombre completo</label>
-          <input v-model="form.nombreC" class="form-input" id="nombreUsuario" required>
-          <label >Nombre de usuario</label>
-          <input v-model="form.nombreU" class="form-input" id="nombreU" required>
-          <label >Correo electronico</label>
-          <input v-model="form.email" class="form-input" id="email" required>
-          <label >Fecha de nacimiento</label>
-          <input v-model="form.fecha" class="form-input" id="fechaN" required>
-          <label >Carrera</label>
-          <input v-model="form.carrera" class="form-input" id="carrera" required>
+        <br />
+        <form id>
+          <label>Nombre completo</label>
+          <input
+            v-model="form.nombreC"
+            class="form-input"
+            id="nombreUsuario"
+            required
+          />
+          <label>Nombre de usuario</label>
+          <input
+            v-model="form.nombreU"
+            class="form-input"
+            id="nombreU"
+            required
+          />
+          <label>Correo electronico</label>
+          <input
+            type="email"
+            v-model="form.email"
+            class="form-input"
+            id="email"
+            required
+          />
+          <label>Fecha de nacimiento</label>
+          <input
+            type="date"
+            v-model="form.fecha"
+            class="form-input"
+            id="fechaN"
+            required
+          />
+          <label>Carrera</label>
+          <input
+            type="text"
+            v-model="form.carrera"
+            class="form-input"
+            id="carrera"
+            required
+          />
           <label></label>
-          <label >Contraseña</label>
-          <input v-model="form.password" class="form-input" type="password" id="password" placeholder="Contraseña">
-          <label >Confirmar Contraseña</label>
-          <input v-model="form.cpassword" class="form-input" type="cpassword" id="cpassword" placeholder="Confirmar Contraseña">
-          </form>
-        <router-link :to="{name: 'login'}"><button class="button button1">Registrarme</button></router-link>
+          <label>Contraseña</label>
+          <input
+            type="password"
+            v-model="form.password"
+            class="form-input"
+            id="password"
+            placeholder="Contraseña"
+          />
+          <label>Confirmar Contraseña</label>
+          <input
+            type="password"
+            v-model="form.cpassword"
+            class="form-input"
+            id="cpassword"
+            placeholder="Confirmar Contraseña"
+          />
+        </form>
+        <button type="submit" class="button button1">Registrarme</button>
       </div>
     </div>
   </div>
@@ -33,49 +73,75 @@ export default {
   name: "Login",
   data() {
     return {
-      form:{
-        nombreC:"",
-        nombreU:"",
-        email:"",
-        fecha:"",
-        carrera:"",
-        password:"",
-        cpassword:""
+      errors: [],
+      form: {
+        nombreC: "",
+        nombreU: "",
+        email: "",
+        fecha: "",
+        carrera: "",
+        password: "",
+        cpassword: "",
+      },
+    };
+  },
+  methods: {
+    checkUser: function(e) {
+      if (
+        this.form.nombreC &&
+        this.form.nombreU &&
+        this.form.email &&
+        this.form.fecha &&
+        this.form.carrera &&
+        this.form.password &&
+        this.form.cpassword
+      ) {
+        return true;
+      } else {
+        this.errors.push();
       }
-    }
-  }
+      e.preventDefault();
+      console.log(this.errors);
+    },
+    sendNewUser: function(checkpoint) {
+      if (checkpoint) {
+        //enviar usuario
+      } else {
+        //mostrar error
+      }
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
-body{
-  background: #4191E0;
+body {
+  background: #4191e0;
   display: flex;
 }
 
-.divlogin{ 
-    //background: url(../img/fondo.jpg) no-repeat; 
-    background-color:  #4191E0 ;
-    width: 100%;
-    display: flex;
-    align-content: center;
-    align-items: center;
-
+.divlogin {
+  //background: url(../img/fondo.jpg) no-repeat;
+  background-color: #4191e0;
+  width: 100%;
+  display: flex;
+  align-content: center;
+  align-items: center;
 }
-.container{ 
-    width: 100%;
-    text-align: center;
+.container {
+  width: 100%;
+  text-align: center;
 }
-.divcont{
-    max-width: 50%;
-    max-height: 70%;
-    background: white;
-    margin: 30px;
-    padding: 2%;
-    border-radius: 10px;
-    display: inline-block;
+.divcont {
+  max-width: 50%;
+  max-height: 70%;
+  background: white;
+  margin: 30px;
+  padding: 2%;
+  border-radius: 10px;
+  display: inline-block;
 }
-input{
+input {
   width: 100%;
   padding: 10px 10px;
   margin: 1px 0;
@@ -83,17 +149,17 @@ input{
   border-radius: 4px;
   border: 2px solid #ccc;
 }
-.input1{
-  width: 50%
+.input1 {
+  width: 50%;
 }
-label{
+label {
   font-size: 70%;
 }
-form{
+form {
   text-align: left;
 }
 button {
-  background-color:#4191E0; 
+  background-color: #4191e0;
   border: none;
   border-radius: 4px;
   color: white;
@@ -107,12 +173,12 @@ button {
   cursor: pointer;
 }
 .button1 {
-  background-color: white; 
-  color: black; 
-  border: 1px solid #4191E0;
+  background-color: white;
+  color: black;
+  border: 1px solid #4191e0;
 }
 .button1:hover {
-  background-color: #4191E0;
+  background-color: #4191e0;
   color: white;
 }
 </style>
