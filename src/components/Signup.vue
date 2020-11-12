cdd7e841c0ae8810434bcbb1f33acdbf4ea5c1ec
vhuepad <50886750+vhuepad@users.noreply.github.com>
2020-11-11 20:40:35 -0500

    Revert "Auth"

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -2,70 +2,53 @@
   <div class="divlogin">
     <div class="container">
       <div class="divcont">
-        <img class="logo" alt="Dayscool logo" src="../assets/Logo512px.svg" />
+        <img alt="Dayscool logo" src="../assets/logod.png" />
         <h3>Crear una cuenta de Dayscool</h3>
-        <form>
-          <div class="line-form">
-            <div class="cell-form">
-              <label>Nombre completo</label>
-              <input
-                v-model="form.nombreC"
-                class="form-input"
-                id="nombreUsuario"
-                required
-              />
-            </div>
-            <div class="cell-form">
-              <label>Nombre de usuario</label>
-              <input
-                v-model="form.nombreU"
-                class="form-input"
-                id="nombreU"
-                required
-              />
-            </div>
-          </div>
-          <div class="line-form">
-            <div class="cell-form">
-              <label>Correo electronico</label>
-              <input
-                type="email"
-                v-model="form.email"
-                class="form-input"
-                id="email"
-                required
-              />
-            </div>
-            <div class="cell-form">
-              <label>Fecha de nacimiento</label>
-              <input
-                type="date"
-                v-model="form.fecha"
-                class="form-input"
-                id="fechaN"
-                required
-              />
-            </div>
-          </div>
-          <div class="line-form">
-            <div class="cell-form">
-              <label>Carrera</label>
-              <input
-                type="text"
-                v-model="form.carrera"
-                class="form-input"
-                id="carrera"
-                required
-              />
-            </div>
-            <div class="cell-form">
-              <label>Rol</label>
-              <select name="rol" id="rol" v-model="form.rol">
-                <option>Profesor</option>
-                <option>Estudiante</option>
-              </select>
-            </div>
-          </div>
+        <br />
+        <form id>
+          <label>Nombre completo</label>
+          <input
+            v-model="form.nombreC"
+            class="form-input"
+            id="nombreUsuario"
+            required
+          />
+          <label>Nombre de usuario</label>
+          <input
+            v-model="form.nombreU"
+            class="form-input"
+            id="nombreU"
+            required
+          />
+          <label>Correo electronico</label>
+          <input
+            type="email"
+            v-model="form.email"
+            class="form-input"
+            id="email"
+            required
+          />
+          <label>Fecha de nacimiento</label>
+          <input
+            type="date"
+            v-model="form.fecha"
+            class="form-input"
+            id="fechaN"
+            required
+          />
+          <label>Carrera</label>
+          <input
+            type="text"
+            v-model="form.carrera"
+            class="form-input"
+            id="carrera"
+            required
+          />
+          <label>Rol</label>
+          <select name="rol" id="rol" v-model="form.rol">
+            <option>Profesor</option>
+            <option>Estudiante</option>
+          </select>
           <label>Contraseña</label>
           <input
             type="password"
@@ -92,8 +75,7 @@
 </template>
 
 <script>
-//import axios from "axios";
-import { mapActions } from 'vuex';
+import axios from "axios";
 export default {
   name: "signup",
   data() {
@@ -107,8 +89,8 @@ export default {
         rol: "",
         carrera: "",
         password: "",
-        cpassword: "",
-      },
+        cpassword: ""
+      }
     };
   },
   methods: {
@@ -129,42 +111,38 @@ export default {
       e.preventDefault();
       console.log(this.errors);
     },
-    ...mapActions(['register']),
-    sendNewUser: function() {
+    sendNewUser() {
       if (this.form.username != "" && this.form.password != "") {
-        /*axios
-          .post(this.$store.state.backURL, {
-            query: `
+      axios
+        .post(this.$store.state.backURL, {
+          query: `
             mutation createUser2($user2: User2Input!){
               createUser2(user2: $user2) {
                 id
-                usernames
+                username
                 role
               }
             }`,
-            variables: {
-              user2: {
-                username: this.form.nombreU,
-                mail: this.form.email,
-                birthDate: this.form.fecha,
-                career: this.form.carrera,
-                role: this.form.rol,
-                name: this.form.nombreC,
-                password: this.form.password,
-              },
+          variables: {
+            user2: {
+              username: this.form.nombreU,
+              mail: this.form.email,
+              birthDate: this.form.fecha,
+              career: this.form.carrera,
+              role: this.form.rol,
+              name: this.form.nombreC,
+              password: this.form.password,
             },
-          })*/
-          this.register(this.form)
-          .then((response) => {
-            console.log(response.data.data);
-            alert("Usuario registrado");
-            this.$router.push('/login');
-          })
-          .catch((err) => {
-            console.log(err);
-            alert("Nombre de usuario ya registrado");
-          });
-      } else {
+          },
+        })
+        .then((response) => {
+          console.log(response.data.data);
+          alert("Usuario registrado")
+        })
+        .catch((err) => {
+          console.log(err)
+          alert("Nombre de usuario ya registrado")});
+      } else  {
         alert("Datos incompletos");
       }
     },
@@ -177,11 +155,15 @@ body {
   background: #4191e0;
   display: flex;
 }
-form {
+
+.divlogin {
+  //background: url(../img/fondo.jpg) no-repeat;
+  background-color: #4191e0;
   width: 100%;
-}
-input {
-  max-width: 25vw;
+  padding: 0%;
+  display: flex;
+  align-content: center;
+  align-items: center;
 }
 .container {
   width: 100%;
@@ -189,25 +171,54 @@ input {
   text-align: center;
 }
 .divcont {
-  width: 50vw;
+  max-width: 50%;
+  max-height: 70%;
   background: white;
   margin: 30px;
   padding: 2%;
   border-radius: 10px;
   display: inline-block;
-  max-height: 85vh;
+}
+input,
+select {
+  width: 100%;
+  padding: 5px 5px;
+  margin: 1px 0;
+  box-sizing: border-box;
+  border-radius: 4px;
+  border: 2px solid #ccc;
+  text-decoration-color: gray;
 }
 .input1 {
   width: 50%;
 }
-.line-form {
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  margin: 5px;
+label {
+  font-size: 70%;
 }
-.cell-form {
-  width: 40%;
+form {
+  text-align: left;
+}
+button {
+  background-color: #4191e0;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  padding: 2%;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 8px 14px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+.button1 {
+  background-color: white;
+  color: black;
+  border: 1px solid #4191e0;
+}
+.button1:hover {
+  background-color: #4191e0;
+  color: white;
 }
 </style>
